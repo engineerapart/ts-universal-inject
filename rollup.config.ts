52112e03100f8d46e0fec0d925f055f16781dd5b
codonward <patrick@engineerapart.com>
2018-06-18 00:17:49 -0500

    fix(release): deploy cjs and es modules, not umd; use 'types' in package.json

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,19 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
-import camelCase from 'lodash.camelcase';
+// import camelCase from 'lodash.camelcase';
 import typescript from 'rollup-plugin-typescript2';
 import json from 'rollup-plugin-json';
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
 
 const pkg = require('./package.json');
-const libraryName = 'ts-universal-inject';
+// const libraryName = 'ts-universal-inject';
 
 export default {
   input: `src/index.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    // { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    { file: pkg.main, format: 'cjs' },
     { file: pkg.module, format: 'es', sourcemap: true }
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
